a4b6f6b2d381d8fe516e6fbf49482bf36a532cb8
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-03-30 11:13:19 +0530

    ADD child component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,6 +4,7 @@ import { NumberBtns, operatorBtns } from './buttons';
 function Calculator() {
   return (
     <div className="calcy-container">
+      <h2>Let&apos;s do some math!</h2>
       <div className="calcy-box">
         <div className="displayvalue">0</div>
         <div className="Number_container">
