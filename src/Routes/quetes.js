ddb33a97e612ad36cbeffe685cfb576a54c0588d
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-04-04 14:58:33 +0530

    ADD: router for the calcutor project

---
+++ src/Routes/quetes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import QuoteComponent from '../components/Quotes';
+
+export default function Quotes() {
+  return (
+    <>
+      <QuoteComponent />
+    </>
+  );
+}
---
+++ src/components/head.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './navbar';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Math Magicians</h1>
+      <Navbar />
+    </div>
+  );
+}
